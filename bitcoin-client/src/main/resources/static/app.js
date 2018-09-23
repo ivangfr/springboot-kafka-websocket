2f71c26bd3989b34cff7f1d33b3c30eb3618eb2d
Ivan Franchin <ivan.franchin@takeaway.com>
2018-09-24 01:05:30 +0200

    improve bitcoin-client interface

--- bitcoin-client/src/main/resources/static/app.js
+++ bitcoin-client/src/main/resources/static/app.js
@@ -4,13 +4,25 @@ function connect() {
     var socket = new SockJS('/price-websocket');
     stompClient = Stomp.over(socket);
 
+    var prevPriceValue = null;
     stompClient.connect({},
         function (frame) {
             console.log('Connected: ' + frame);
             stompClient.subscribe('/topic/price', function (price) {
                 var priceBody = JSON.parse(price.body);
-                var record = '<p>'+priceBody.value+'/'+priceBody.timestamp+'</p>';
-                $('#priceList').prepend(record);
+                var priceValue = priceBody.value;
+                var priceTimestamp = priceBody.timestamp;
+                if (prevPriceValue == null) {
+                    prevPriceValue = priceValue;
+                }
+                var priceVar = priceValue - prevPriceValue;
+                prevPriceValue = priceValue;
+
+                $('#currentPrice').text(Number(priceValue).toFixed(2));
+                $('#variation').text((priceVar > 0 ? "+" : "") + Number(priceVar).toFixed(2));
+
+                var row = '<tr><td>'+priceValue+'</td><td>'+priceTimestamp+'</td></tr>';
+                $('#priceList tbody').prepend(row);
             });
         },
         function() {
