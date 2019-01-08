6aa19c59832874fd2b1654432f4f0ae548797dfc
Ivan Franchin <ivan.franchin@takeaway.com>
2019-01-08 17:58:42 -0200

    changes: - update spring-boot version to 2.0.7; - using Payload annotation to get the payload from a message; - add spring-cloud dependency to parent pom; - remove org.springframework.kafka:spring-kafka dependency; - update README.

--- bitcoin-client/src/main/java/com/mycompany/bitcoinclient/bus/PriceStream.java
+++ bitcoin-client/src/main/java/com/mycompany/bitcoinclient/bus/PriceStream.java
@@ -7,8 +7,8 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.integration.IntegrationMessageHeaderAccessor;
 import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
@@ -26,12 +26,12 @@ public class PriceStream {
     }
 
     @StreamListener(Sink.INPUT)
-    public void handlePriceDto(Message<String> message,
+    public void handlePriceDto(@Payload String message,
                                @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
-                               @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
-                               @Header(KafkaHeaders.OFFSET) long offset,
-                               @Header(IntegrationMessageHeaderAccessor.DELIVERY_ATTEMPT) int deliveryAttempt) {
-        PriceDto priceDto = gson.fromJson(message.getPayload(), PriceDto.class);
+                               @Header(KafkaHeaders.RECEIVED_PARTITION_ID) Integer partition,
+                               @Header(KafkaHeaders.OFFSET) Long offset,
+                               @Header(IntegrationMessageHeaderAccessor.DELIVERY_ATTEMPT) Integer deliveryAttempt) {
+        PriceDto priceDto = gson.fromJson(message, PriceDto.class);
         log.info("PriceDto with value '{}' and timestamp '{}' received from bus. topic: {}, partition: {}, offset: {}, deliveryAttempt: {}",
                 priceDto.getValue(), priceDto.getTimestamp(), topic, partition, offset, deliveryAttempt);
 
