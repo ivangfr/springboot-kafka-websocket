ea8da3a2ab2a80e9a88932eadceb6807c1b3a526
Ivan Franchin <ivan.franchin@takeaway.com>
2021-05-29 16:31:00 +0200

    project update - update to spring-boot 2.5.0; - update to spring-cloud 2020.0.3-SNAPSHOT; - update to springdoc openapi 1.5.9; - update to jib 3.0.0; - rename docker image prefix to ivanfranchin; - rename some properties in pom.xml files; - add lombok-mapstruct-binding to maven-compiler-plugin annotationProcessorPaths; - update maven-wrapper.properties; - update mysql docker image to 8.0.25; - update to confluentinc docker images 6.1.1

--- start-apps.sh
+++ start-apps.sh
@@ -3,20 +3,20 @@
 echo
 echo "Starting bitcoin-api..."
 
-docker run -d --rm --name bitcoin-api \
-  -p 9081:8080 --network=springboot-kafka-websocket_default \
+docker run -d --rm --name bitcoin-api -p 9081:8080 \
   -e MYSQL_HOST=mysql -e KAFKA_HOST=kafka -e KAFKA_PORT=9092 -e ZIPKIN_HOST=zipkin \
+  --network=springboot-kafka-websocket_default \
   --health-cmd="curl -f http://localhost:8080/actuator/health || exit 1" --health-start-period=1m \
-  docker.mycompany.com/bitcoin-api:1.0.0
+  ivanfranchin/bitcoin-api:1.0.0
 
 echo
 echo "Starting bitcoin-client..."
 
-docker run -d --rm --name bitcoin-client \
-  -p 9082:8080 --network=springboot-kafka-websocket_default \
+docker run -d --rm --name bitcoin-client -p 9082:8080 \
   -e KAFKA_HOST=kafka -e KAFKA_PORT=9092 -e ZIPKIN_HOST=zipkin \
+  --network=springboot-kafka-websocket_default \
   --health-cmd="curl -f http://localhost:8080/actuator/health || exit 1" --health-start-period=1m \
-  docker.mycompany.com/bitcoin-client:1.0.0
+  ivanfranchin/bitcoin-client:1.0.0
 
 printf "\n"
 printf "%15s | %37s |\n" "Application" "URL"
