e97a05327cb2cf6076dac2ab86c71e4083a82fc5
Ivan Franchin <ivan.franchin@takeaway.com>
2024-12-27 17:34:29 -0300

    Project upgrade
    - Upgrade to Spring Boot 3.4.1;
    - Upgrade to Spring Cloud 2024.0.0;
    - Upgrade to SpringDoc OpenAPI 2.7.0;
    - Refactor pom.xml and docker-compose.yml files;
    - Upgrade to MySQL docker image 9.1.0;
    - Upgrade to Confluentinc docker images 7.8.0;
    - Upgrade to Kafdrop docker image 4.0.2;
    - Rename docker-build.sh to build-docker-images.sh;
    - Update README file.

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -11,5 +11,12 @@ BITCOIN_API_DOCKER_IMAGE_NAME="${DOCKER_IMAGE_PREFIX}/${BITCOIN_API_APP_NAME}:${
 
 SKIP_TESTS="true"
 
-./mvnw clean spring-boot:build-image --projects "$BITCOIN_CLIENT_APP_NAME" -DskipTests="$SKIP_TESTS" -Dspring-boot.build-image.imageName="$BITCOIN_CLIENT_DOCKER_IMAGE_NAME"
-./mvnw clean spring-boot:build-image --projects "$BITCOIN_API_APP_NAME" -DskipTests="$SKIP_TESTS" -Dspring-boot.build-image.imageName="$BITCOIN_API_DOCKER_IMAGE_NAME"
+./mvnw clean spring-boot:build-image \
+  --projects "$BITCOIN_CLIENT_APP_NAME" \
+  -DskipTests="$SKIP_TESTS" \
+  -Dspring-boot.build-image.imageName="$BITCOIN_CLIENT_DOCKER_IMAGE_NAME"
+
+./mvnw clean spring-boot:build-image \
+  --projects "$BITCOIN_API_APP_NAME" \
+  -DskipTests="$SKIP_TESTS" \
+  -Dspring-boot.build-image.imageName="$BITCOIN_API_DOCKER_IMAGE_NAME"
