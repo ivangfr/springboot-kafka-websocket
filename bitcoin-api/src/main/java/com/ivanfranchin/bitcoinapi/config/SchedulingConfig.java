48c23ae7f01cc5dc8e073d03caa33a24b68b7348
Ivan Franchin <ivan.franchin@takeaway.com>
2024-12-27 18:31:12 -0300

    Organize and refactor the scheduler

--- bitcoin-api/src/main/java/com/ivanfranchin/bitcoinapi/config/SchedulingConfig.java
+++ bitcoin-api/src/main/java/com/ivanfranchin/bitcoinapi/config/SchedulingConfig.java
@@ -1,9 +1,9 @@
-package com.ivanfranchin.bitcoinapi.scheduler;
+package com.ivanfranchin.bitcoinapi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @Configuration
-public class SchedulerConfig {
+public class SchedulingConfig {
 }
--- bitcoin-api/src/main/java/com/ivanfranchin/bitcoinapi/price/PriceScheduler.java
+++ bitcoin-api/src/main/java/com/ivanfranchin/bitcoinapi/price/PriceScheduler.java
@@ -1,8 +1,6 @@
-package com.ivanfranchin.bitcoinapi.scheduler;
+package com.ivanfranchin.bitcoinapi.price;
 
-import com.ivanfranchin.bitcoinapi.price.PriceEventEmitter;
 import com.ivanfranchin.bitcoinapi.price.model.Price;
-import com.ivanfranchin.bitcoinapi.price.PriceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
